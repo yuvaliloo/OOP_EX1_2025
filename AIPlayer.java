2a8d0941b2ff4a8da677dbb5ba9eb0dca3f54aeb
Moshe Ofer <mosheofer1@gmail.com>
2024-11-05 09:56:54 +0200

    Update AIPlayer.java

--- AIPlayer.java
+++ AIPlayer.java
@@ -8,7 +8,6 @@ public abstract class AIPlayer extends Player {
     public static void registerAllAIPlayers() {
         registerAIPlayerType("RandomAI", RandomAI.class);
         registerAIPlayerType("GreedyAI", GreedyAI.class);
-        registerAIPlayerType("MinMaxAI", MinMaxAI.class);
         // Add more AIPlayer subclasses here as needed
     }
     @Override
